0b28a79d54c2b14a7a1928c59f0ac89be9bb1d63
Yanettte Abzun <yannettee.a@gmail.com>
2016-09-02 14:17:35 -0700

    finished lettering

--- battleship.js
+++ battleship.js
@@ -21,6 +21,10 @@ var letterConversion = {
 	"J": 9
 }
 
+var letterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "j"]
+
+
+
 // makes the grid columns and rows
 for (i = 0; i < cols; i++) {
 	for (j = 0; j < rows; j++) {
@@ -34,6 +38,9 @@ for (i = 0; i < cols; i++) {
 		square.className = "boardSquare";
 
 		// THIS IS WHERE YOU WILL ADD CODE FOR PART 1 TO ADD TEXT TO EACH SQUARE
+
+		square.textContent = i + 1;
+
 
 		// set each grid square's coordinates: multiples of the current row or column number
 		var topPosition = j * squareSize;
